6082da3704008403e75724f4630bd119d96907ec
17a1 <dtdat.dhkl17a1hn@sv.uneti.edu.vn>
2024-10-07 09:52:41 +0700

    fix p3

--- bt2.9.py
+++ bt2.9.py
@@ -1,11 +1,11 @@
 import json
 
-# Tạo một từ điển Python (bắt đầu từ Python 3.7, từ điển giữ thứ tự các khóa theo thứ tự thêm vào)
+# Tạo một từ điển Python 
 python_dict = {
-    "name": "John",
-    "age": 30,
-    "city": "New York",
-    "job": "Engineer"
+    "name": "tuấn Đạt",
+    "age": 19,
+    "city": "Hà Nam",
+    "job": "Analyst"
 }
 
 # Chuyển đổi từ điển thành chuỗi JSON với mức thụt lề 4
